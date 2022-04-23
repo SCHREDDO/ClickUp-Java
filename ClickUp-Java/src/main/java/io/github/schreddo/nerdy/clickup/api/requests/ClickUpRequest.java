afadb8942cff20c9c8e7c550afb218b5e487fd7d
Sebastian Lühnen <seb-lue@t-online.de>
2022-04-23 21:52:32 +0200

    add a abstract class for requests

---
+++ ClickUp-Java/src/main/java/io/github/schreddo/nerdy/clickup/api/requests/ClickUpRequest.java
@@ -0,0 +1,43 @@
+// Copyright (C) 2019-2022 Sebastian Lühnen
+//
+//
+// This file is part of ClickUp-Java.
+// 
+// ClickUp-Java is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// ClickUp-Java is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with ClickUp-Java. If not, see <http://www.gnu.org/licenses/>.
+//
+//
+// Created By: Sebastian Lühnen
+// Created On: 23.04.2022
+// Last Edited On: 23.04.2022
+// Language: Java
+//
+package io.github.schreddo.nerdy.clickup.api.requests;
+
+public abstract class ClickUpRequest {
+	
+	private String accessToken;
+	
+	public String getAccessToken() {
+		return accessToken;
+	}
+	public void setAccessToken(String accessToken) {
+		this.accessToken = accessToken;
+	}
+	
+	public ClickUpRequest(String accessToken) {
+		this.setAccessToken(accessToken);
+	}
+	
+	public abstract Object execute();
+}
